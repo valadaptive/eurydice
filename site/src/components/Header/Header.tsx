de2cb273e213d720ab368b43e5d55f8377e02347
gltile <80788438+gltile-two-electric-boogaloo@users.noreply.github.com>
2022-08-02 19:48:41 +0100

    make logo a link to the homepage
    Signed-off-by: gltile <80788438+gltile-two-electric-boogaloo@users.noreply.github.com>

--- site/src/components/Header/Header.tsx
+++ site/src/components/Header/Header.tsx
@@ -52,7 +52,7 @@ class Header extends Component {
             <div className={classNames(style.header, {[style.headerScrolled]: this.state.scroll !== 0})}>
                 <FlexHorizontal class={style.logoDiv}>
                     <Logo height="100%" width="100%" style="width: 1.15rem; height: 1.15rem;"/>
-                    <p>eurydice <sup>v{pkg.version}</sup></p>
+                    <a href="/">eurydice <sup>v{pkg.version}</sup></a>
                 </FlexHorizontal>
                 {
                     (Number(variables.mobileWidth) >= document.body.clientWidth) ?

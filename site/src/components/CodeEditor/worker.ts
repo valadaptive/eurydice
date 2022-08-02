65fbf454aea951e2b82dc92d1b557224bc5acfaf
valadaptive <valadaptive@protonmail.com>
2022-08-02 14:10:34 -0400

    Remove console.log statements

--- site/src/components/CodeEditor/worker.ts
+++ site/src/components/CodeEditor/worker.ts
@@ -3,8 +3,6 @@ import printValue from '../../../../src/print';
 import parse from '../../../../src/parse';
 
 self.onmessage = ({data: {prog}}): void => {
-    console.log(prog);
-
     try {
         const evaluated = evaluate(parse(prog));
         self.postMessage({
@@ -12,8 +10,6 @@ self.onmessage = ({data: {prog}}): void => {
             output: printValue(evaluated)
         });
     } catch (err) {
-        console.log(err);
-
         self.postMessage({
             success: false,
             output: (err as Error).message
